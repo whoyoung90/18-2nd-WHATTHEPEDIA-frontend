d94b5e44792af3227330fed3fd3cff2690b7d70b
wooyoungkim <whoyoung90@naver.com>
2021-03-29 19:39:02 +0900

    Add: 초기세팅 완료

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import './Styles/reset.scss';
 import './Styles/common.scss';
+import theme from './theme';
 
-ReactDOM.render(<Routes />, document.getElementById('root'));
+const GlobalStyle = createGlobalStyle`
+*{
+  margin: 0;
+  padding: 0;
+}
+
+body{
+  box-sizing: border-box;
+}
+
+button:hover{
+  cursor: pointer;
+}
+`;
+
+ReactDOM.render(
+  <>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <Routes />
+    </ThemeProvider>
+  </>,
+  document.getElementById('root')
+);
