e1694a42d17923e7fa0feb4d20e169b3c940984f
sunaaank <suna.namgung@gmail.com>
2021-04-02 12:29:55 +0900

    Fix : Conflict 해결
    Fix : 레이아웃 오류 수정

--- src/Routes.js
+++ src/Routes.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-// import Main from './Pages/Main/Main';
+import Main from './Pages/Main/Main';
 import Movie from './Pages/Movie/Movie';
 import MyPage from './Pages/MyPage/MyPage';
 import Review from './Pages/Review/Review';
-import PublicFooter from './Components/PublicFooter/PublicFooter';
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={PublicFooter} />
+          <Route exact path="/" component={Main} />
           <Route exact path="/movie" component={Movie} />
           <Route exact path="/mypage" component={MyPage} />
           <Route exact path="/review" component={Review} />
